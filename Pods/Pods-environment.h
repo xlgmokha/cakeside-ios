6ff632a1444a214ba61597ae9e1a034e5f1f6073
mo khan <mo@mokhan.ca>
2013-07-14 15:17:39 -0600

    add more pods

--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -12,6 +12,24 @@
 #define COCOAPODS_VERSION_MINOR_AFNetworking 3
 #define COCOAPODS_VERSION_PATCH_AFNetworking 1
 
+// BlockAlertsAnd-ActionSheets
+#define COCOAPODS_POD_AVAILABLE_BlockAlertsAnd_ActionSheets
+#define COCOAPODS_VERSION_MAJOR_BlockAlertsAnd_ActionSheets 1
+#define COCOAPODS_VERSION_MINOR_BlockAlertsAnd_ActionSheets 0
+#define COCOAPODS_VERSION_PATCH_BlockAlertsAnd_ActionSheets 6
+
+// BlockAlertsAnd-ActionSheets/TableAlertView
+#define COCOAPODS_POD_AVAILABLE_BlockAlertsAnd_ActionSheets_TableAlertView
+#define COCOAPODS_VERSION_MAJOR_BlockAlertsAnd_ActionSheets_TableAlertView 1
+#define COCOAPODS_VERSION_MINOR_BlockAlertsAnd_ActionSheets_TableAlertView 0
+#define COCOAPODS_VERSION_PATCH_BlockAlertsAnd_ActionSheets_TableAlertView 6
+
+// BlocksKit
+#define COCOAPODS_POD_AVAILABLE_BlocksKit
+#define COCOAPODS_VERSION_MAJOR_BlocksKit 1
+#define COCOAPODS_VERSION_MINOR_BlocksKit 8
+#define COCOAPODS_VERSION_PATCH_BlocksKit 2
+
 // MBProgressHUD
 #define COCOAPODS_POD_AVAILABLE_MBProgressHUD
 #define COCOAPODS_VERSION_MAJOR_MBProgressHUD 0
@@ -24,9 +42,21 @@
 #define COCOAPODS_VERSION_MINOR_SSKeychain 1
 #define COCOAPODS_VERSION_PATCH_SSKeychain 0
 
+// SVPullToRefresh
+#define COCOAPODS_POD_AVAILABLE_SVPullToRefresh
+#define COCOAPODS_VERSION_MAJOR_SVPullToRefresh 0
+#define COCOAPODS_VERSION_MINOR_SVPullToRefresh 4
+#define COCOAPODS_VERSION_PATCH_SVPullToRefresh 1
+
 // TSMessages
 #define COCOAPODS_POD_AVAILABLE_TSMessages
 #define COCOAPODS_VERSION_MAJOR_TSMessages 0
 #define COCOAPODS_VERSION_MINOR_TSMessages 9
 #define COCOAPODS_VERSION_PATCH_TSMessages 3
+
+// libffi
+#define COCOAPODS_POD_AVAILABLE_libffi
+#define COCOAPODS_VERSION_MAJOR_libffi 3
+#define COCOAPODS_VERSION_MINOR_libffi 0
+#define COCOAPODS_VERSION_PATCH_libffi 13
 
--- cakeside-ios/controllers/LoginViewController.m
+++ cakeside-ios/controllers/LoginViewController.m
@@ -8,9 +8,11 @@
 
 #import "LoginViewController.h"
 #import "SSKeychain.h"
+#import "MBProgressHUD.h"
+#import "AFNetworking.h"
 
 @interface LoginViewController ()<UITextFieldDelegate>
-
+@property (strong, nonatomic) MBProgressHUD *HUD;
 @end
 
 @implementation LoginViewController
@@ -45,14 +47,142 @@
   {
     self.passwordTextBox.text = password;
   }
+}
 
-  
+- (MBProgressHUD *)HUD
+{
+  if (!_HUD) {
+    _HUD = [[MBProgressHUD alloc] initWithView:self.view];
+    [self.view addSubview:_HUD];
+  }
+  return _HUD;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark UITextFieldDelegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+  // user did hit the Return/Done button on the keyboard
+  if (textField == self.emailTextBox)
+  {
+    // goto password field
+    [self.passwordTextBox becomeFirstResponder];
+  }
+  else
+    if (textField == self.passwordTextBox)
+    {
+      [self login:self.passwordTextBox];
+    }
+  
+  return YES;
+}
+
+
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+  [textField resignFirstResponder];
+}
+
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+  [UIView animateWithDuration:HUD_ANIMATION_DURATION animations:^{
+//    self.contentView.$y = -116;
+  }];
+}
+
+
+- (void)dismissKeyboard
+{
+  [self.view endEditing:YES];
+  /*
+  if (self.contentView.$y!=0)
+  {
+    [UIView animateWithDuration:HUD_ANIMATION_DURATION animations:^{
+      self.contentView.$y = 0;
+    }];
+  }
+  */
+}
+
+#pragma mark - Action methods
+
+
+- (IBAction)backgroundTapped:(id)sender
+{
+  [self dismissKeyboard];
+}
+
+- (IBAction)login:(id)sender
+{
+  //    [self performSegueWithIdentifier:@"gotoMainInterface" sender:nil];
+  //    return;
+  
+  [self dismissKeyboard];
+  self.HUD.mode = MBProgressHUDModeIndeterminate;
+  self.HUD.labelText = nil;
+  self.HUD.customView = nil;
+  [self.HUD show:YES];
+  
+  
+  // try to login/register...
+  NSDictionary *params = @{@"email": self.emailTextBox.text, @"password": self.passwordTextBox.text};
+  
+  AFHTTPClient *httpClient = [[AFHTTPClient alloc] initWithBaseURL:[NSURL URLWithString:HOST]];
+  [httpClient registerHTTPOperationClass:[AFJSONRequestOperation class]];
+  [httpClient setDefaultHeader:@"Accept" value:@"application/json"];
+  [httpClient.operationQueue setMaxConcurrentOperationCount:1];
+  [httpClient setParameterEncoding:AFJSONParameterEncoding];
+  NSMutableURLRequest *request = [httpClient requestWithMethod:@"POST" path:URL_LOGIN parameters:params];
+  
+  NSLog(@"POST: %@", request);
+  
+  AFJSONRequestOperation *operation = [AFJSONRequestOperation
+                                       JSONRequestOperationWithRequest:request
+                                       success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON)
+                                       {
+                                         NSLog(@"%@", JSON);
+                                         
+                                         // check if login was successfull
+                                         NSString *token = [JSON objectForKey:@"auth_token"];
+                                         if (!token || [token isEmpty])
+                                         {
+                                           [self.HUD hide:YES];
+                                           [TSMessage showNotificationInViewController:self
+                                                                             withTitle:@"Login failed!"
+                                                                           withMessage:@"Please verify your login credentials and try again."
+                                                                              withType:TSMessageNotificationTypeError
+                                                                          withDuration:OVERLAY_MESSAGE_DURATION
+                                            ];
+                                         }
+                                         else
+                                         {
+                                           // successfull login!
+                                           [SSKeychain setPassword:token forService:KEYCHAIN_API_TOKEN account:KEYCHAIN_ACCOUNT];
+                                           [SSKeychain setPassword:self.email.text forService:KEYCHAIN_USER_NAME account:KEYCHAIN_ACCOUNT];
+                                           [SSKeychain setPassword:self.password.text forService:KEYCHAIN_USER_PASSWORD account:KEYCHAIN_ACCOUNT];
+                                           
+                                           [self performSegueWithIdentifier:@"gotoMainInterface" sender:nil];
+                                         }
+                                         
+                                       }
+                                       failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON)
+                                       {
+                                         [self.HUD hide:YES];
+                                         NSLog(@"Request Failed with Error: %@, %@", error, error.userInfo);
+                                         
+                                         BlockAlertView *alert = [BlockAlertView alertWithTitle:@"Something's not working." message:@"Login/Signup currently not possible. Please try again."];
+                                         [alert setCancelButtonWithTitle:@"Ok" block:nil];
+                                         [alert show];
+                                       }];
+  [operation start];
+  
 }
 
 @end
