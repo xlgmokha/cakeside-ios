22dcb7a7d71bd36d2b5ab333ec83dd15badb618d
mo khan <mo@mokhan.ca>
2013-07-14 14:49:13 -0600

    add SSKeychain

--- cakeside-ios/LoginViewController.h
+++ cakeside-ios/LoginViewController.h
@@ -9,5 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface LoginViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UITextField *emailTextBox;
+@property (strong, nonatomic) IBOutlet UITextField *passwordTextBox;
+@property (strong, nonatomic) IBOutlet UIButton *loginButton;
 
 @end
--- cakeside-ios/LoginViewController.m
+++ cakeside-ios/LoginViewController.m
@@ -8,7 +8,7 @@
 
 #import "LoginViewController.h"
 
-@interface LoginViewController ()
+@interface LoginViewController ()<UITextFieldDelegate>
 
 @end
 
@@ -18,6 +18,34 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+  UIView *emailPaddingView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 5, 20)];
+  self.emailTextBox.leftView = emailPaddingView;
+  self.emailTextBox.leftViewMode = UITextFieldViewModeAlways;
+  
+  UIView *passwordPaddingView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 5, 20)];
+  self.passwordTextBox.leftView = passwordPaddingView;
+  self.passwordTextBox.leftViewMode = UITextFieldViewModeAlways;
+  
+//  self.contentView.$y = 0;
+  
+  // prepopulate username/password with stored information
+  /*
+  NSError *error;
+  NSString *username = [SSKeychain passwordForService:KEYCHAIN_USER_NAME account:KEYCHAIN_ACCOUNT error:&error];
+  if (error) { username = nil; }
+  NSString *password = [SSKeychain passwordForService:KEYCHAIN_USER_PASSWORD account:KEYCHAIN_ACCOUNT error:&error];
+  if (error) { password = nil; }
+  
+  if (username)
+  {
+    self.emailTextBox.text = username;
+  }
+  if (password)
+  {
+    self.passwordTextBox.text = password;
+  }
+  */
+  
 }
 
 - (void)didReceiveMemoryWarning
