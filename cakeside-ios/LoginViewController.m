6637d4ccb317185e63aab273f32b6849d3ab5e43
mo khan <mo@mokhan.ca>
2013-07-14 14:26:04 -0600

    rename to view controller to login view controller

--- cakeside-ios/LoginViewController.m
+++ cakeside-ios/LoginViewController.m
@@ -6,13 +6,13 @@
 //  Copyright (c) 2013 mo khan. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "LoginViewController.h"
 
-@interface ViewController ()
+@interface LoginViewController ()
 
 @end
 
-@implementation ViewController
+@implementation LoginViewController
 
 - (void)viewDidLoad
 {
